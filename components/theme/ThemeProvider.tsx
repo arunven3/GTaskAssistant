2573c0033d84438895d36d673e445be518d19016
Arun Venkatesan <67637172+arunven3@users.noreply.github.com>
2025-08-24 19:39:59 +0530

    Adding Theme Provider

---
+++ components/theme/ThemeProvider.tsx
@@ -0,0 +1,9 @@
+import { createTheme } from "flowbite-react";
+import { BaseTheme } from "./BaseTheme";
+import { GreyThemeCorlor } from './colors/grey';
+
+
+export const Theme = createTheme({
+    ...BaseTheme,
+    ...GreyThemeCorlor
+});
